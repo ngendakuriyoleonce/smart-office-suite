d1dd148ce3291bc351def8087a304010855e68da
ngendakuriyoleonce <ndendakuriyoleonce75@gmail.com>
2026-06-27 14:58:26 +0400

    implement employee model

---
+++ app/Models/Employee.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#[Fillable([
+    'user_id',
+    'employee_id',
+    'first_name',
+    'last_name',
+    'hire_date',
+    'status',
+    'department_id',
+    'position_id',
+    'salary',
+    'phone',
+    'avatar',
+])]
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function position(): BelongsTo
+    {
+        return $this->belongsTo(Position::class);
+    }
+}
